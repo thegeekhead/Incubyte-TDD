6effca8d29beffefa692227f5a1883e844fc79b4
Anshansh Suman <anshanshsuman@192.168.1.5>
2025-06-21 09:58:20 +0530

    Step-2 Add method handles unknown amount of numbers

--- stringCalculator.spec.js
+++ stringCalculator.spec.js
@@ -12,3 +12,7 @@ assertEqual(Add(""), 0, "Returns 0 for empty string");
 assertEqual(Add("1"), 1, "Returns 1 for input '1'");
 assertEqual(Add("56"), 56, "Returns 56 for input '56'");
 assertEqual(Add("5,6"), 11, "Returns 11 for input '5,6'");
+
+assertEqual(Add("1,2,3"), 6, "Returns 6 for input '1,2,3'");
+assertEqual(Add("5,5,5,5,5"), 25, "Handles 5 numbers");
+assertEqual(Add("100,200,300,400"), 1000, "Handles 4 large numbers");
