9335085d79c090e90720f0d9b4c81951e70ba9ca
Anshansh Suman <anshanshsuman@192.168.1.5>
2025-06-21 09:48:16 +0530

    Step-1 (handle two strings)

--- stringCalculator.js
+++ stringCalculator.js
@@ -3,7 +3,8 @@ function Add(numbers) {
         return 0;
     }
 
-    return parseInt(numbers)
+    const numArray = numbers.split(",").map(Number);
+    return numArray.reduce((sum, num) => sum + num, 0);
 }
 
 module.exports = Add;
--- stringCalculator.spec.js
+++ stringCalculator.spec.js
@@ -11,3 +11,4 @@ function assertEqual(actual, expected, description) {
 assertEqual(Add(""), 0, "Returns 0 for empty string");
 assertEqual(Add("1"), 1, "Returns 1 for input '1'");
 assertEqual(Add("56"), 56, "Returns 56 for input '56'");
+assertEqual(Add("5,6"), 11, "Returns 11 for input '5,6'");
